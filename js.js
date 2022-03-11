d41879985883e4ce7a35f0848af3a2ba091e257e
Kazi Saheb <kzsaheb@gmail.com>
2022-03-12 00:42:52 +0600

    Function simplified

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
 img = document.getElementById('img')
 let i = 1
-slider = () => {
+setInterval(() => {
   if (i > 3) { i = 1 }
   let url = `imgs/${i}.jpg`
   img.setAttribute('src', url)
   i++
-}
-setInterval(slider, 1000)
+}, 1000)
